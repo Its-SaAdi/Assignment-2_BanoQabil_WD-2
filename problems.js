2f18af996df5706521fc94b66609bb1294e888c2
Its-SaAdi <itssaadi34@gmail.com>
2023-12-27 14:53:16 +0500

    Solved Q1

---
+++ problems.js
@@ -0,0 +1,10 @@
+/* 1. Rewrite the following code using a ternary operator:
+let result;
+if (score >= 80) {
+    result = "Pass";
+} else {
+    result = "Fail";
+} */
+
+// let result = score >= 80 ? "Pass" : "Fail";
+
